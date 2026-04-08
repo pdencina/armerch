8217662f2813c4bff747bef4c61739e520da27aa
Pablo David Encina Acevedo <Pablo.encina@klap.cl>
2026-04-08 14:37:40 -0400

    layaout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner'
 import './globals.css'
 
-const geist = Geist({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'ARM Merch',
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className={geist.className}>
+      <body className={inter.className}>
         {children}
         <Toaster
           theme="dark"
