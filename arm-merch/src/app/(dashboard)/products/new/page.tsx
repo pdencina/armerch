1f6b32820dde7fc56b9d7e97df9e77de80085095
Pablo David Encina Acevedo <Pablo.encina@klap.cl>
2026-04-14 10:20:38 -0400

    nuevos cambios 5

--- arm-merch/src/app/(dashboard)/products/new/page.tsx
+++ arm-merch/src/app/(dashboard)/products/new/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/lib/supabase/server'
 import ProductForm from '../product-form'
 
 export default async function NewProductPage() {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const { data: categories } = await supabase
     .from('categories')
